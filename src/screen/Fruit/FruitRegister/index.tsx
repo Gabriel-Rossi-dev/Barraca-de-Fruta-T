f8fa19c80de6073c1c401832ed3f83897d8e35c3
Gabriel-Rossi-dev <costagabrielrd@gmail.com>
2023-07-09 20:17:58 -0300

    exitRegister function not working

--- src/screen/Fruit/FruitRegister/index.tsx
+++ src/screen/Fruit/FruitRegister/index.tsx
@@ -1,77 +1,109 @@
-import {Card} from 'react-native-paper'
-import { styles } from './styled';
-import {Ionicons} from '@expo/vector-icons'
-import theme from '../../../global/theme/theme';
-import { useNavigation } from '@react-navigation/native';
+import { Card } from "react-native-paper";
+import { styles } from "./styled";
+import { Ionicons } from "@expo/vector-icons";
+import theme from "../../../global/theme/theme";
+import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+  Alert,
+} from "react-native";
 
-
+function exitRegister(navigation: any) {
+  Alert.alert(
+    "Cancelar Cadastro",
+    "Tem certeza que quer cancelar o cadastro da fruta?  Você perderá todas as informações inseridas",
+    [
+      {
+        text: "Não",
+      },
+      {
+        text: "Sim, cancelar",
+        onPress: () => {
+          navigation.navigate("FruitHome");
+        },
+      },
+    ]
+  );
+}
 
 export default function FruitRegister() {
-  const navigation:any = useNavigation
-    return (
-      <View>
-        <View style ={styles.firstCard}>
-      <Card contentStyle = {styles.viewText}>
-        <Ionicons 
-        style = {styles.iconSearch}
-        name= {'nutrition-outline'}
-        size={24}
-        />
-        <TextInput
-          style={styles.InputText}
-          placeholder="Nome da fruta"
-          placeholderTextColor={theme.colors.lightGray}
-        />
-      </Card>
+  const navigation: any = useNavigation;
+  return (
+    <View>
+      <View style={styles.headerStyle}>
+        <Text style={styles.textRegister}>Cadastrar Fruta</Text>
+        <TouchableOpacity onPress={() => exitRegister(navigation)}>
+          <Ionicons
+            style={styles.exit}
+            name={"ios-close"}
+            size={36}
+            color={theme.colors.lightGray}
+          />
+        </TouchableOpacity>
       </View>
-      <View style ={styles.secondCard}>
-      <Card contentStyle = {styles.viewText}>
-        <Ionicons 
-        style = {styles.iconSearch}
-        name= {'cash-outline'}
-        size={24}
-        />
-        <TextInput
-          style={styles.InputText}
-          placeholder="Preço do Kilo"
-          placeholderTextColor={theme.colors.lightGray}
-        />
-      </Card>
+      <View style={styles.firstCard}>
+        <Card contentStyle={styles.viewText}>
+          <Ionicons
+            style={styles.iconSearch}
+            name={"nutrition-outline"}
+            size={24}
+          />
+          <TextInput
+            style={styles.InputText}
+            placeholder="Nome da fruta"
+            placeholderTextColor={theme.colors.lightGray}
+          />
+        </Card>
       </View>
-      <View style ={styles.secondCard}>
-      <Card contentStyle = {styles.viewText}>
-        <Ionicons 
-        style = {styles.iconSearch}
-        name= {'server-outline'}
-        size={24}
-        />
-        <TextInput
-          style={styles.InputText}
-          placeholder="Quantidade no estoque"
-          placeholderTextColor={theme.colors.lightGray}
-        />
-      </Card>
+      <View style={styles.secondCard}>
+        <Card contentStyle={styles.viewText}>
+          <Ionicons style={styles.iconSearch} name={"cash-outline"} size={24} />
+          <TextInput
+            style={styles.InputText}
+            placeholder="Preço do Kilo"
+            placeholderTextColor={theme.colors.lightGray}
+          />
+        </Card>
       </View>
-      <View style ={styles.secondCard}>
-      <Card contentStyle = {styles.viewText}>
-        <Ionicons 
-        style = {styles.iconSearch}
-        name= {'people-outline'}
-        size={24}
-        />
-        <TextInput
-          style={styles.InputText}
-          placeholder="Fornecedor"
-          placeholderTextColor={theme.colors.lightGray}
-        />
-      </Card>
+      <View style={styles.secondCard}>
+        <Card contentStyle={styles.viewText}>
+          <Ionicons
+            style={styles.iconSearch}
+            name={"server-outline"}
+            size={24}
+          />
+          <TextInput
+            style={styles.InputText}
+            placeholder="Quantidade no estoque"
+            placeholderTextColor={theme.colors.lightGray}
+          />
+        </Card>
       </View>
-      <TouchableOpacity style={styles.addSupplier}
-      onPress={() => navigation.navigate('FruitRegister')}>
-        <Text style = {styles.textSupply}>Cadastrar Fruta</Text>
+      <View style={styles.secondCard}>
+        <Card contentStyle={styles.viewText}>
+          <Ionicons
+            style={styles.iconSearch}
+            name={"people-outline"}
+            size={24}
+          />
+          <TextInput
+            style={styles.InputText}
+            placeholder="Fornecedor"
+            placeholderTextColor={theme.colors.lightGray}
+          />
+        </Card>
+      </View>
+      <TouchableOpacity
+        style={styles.addSupplier}
+        onPress={() => navigation.navigate("FruitRegister")}
+      >
+        <Text style={styles.textSupply}>Cadastrar Fruta</Text>
       </TouchableOpacity>
-      </View>
-    );
-  }
+    </View>
+  );
+}
